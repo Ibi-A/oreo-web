90a19eb16cc31aaaa14febbbcd74312abc1fe369
Ibi Andrianoasy <ibi.andrianoasy@gmail.com>
2021-11-13 19:08:44 +0100

    created PostCard component

---
+++ src/components/PostCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card class="mx-auto" max-width="640">
+    <v-carousel>
+      <v-carousel-item
+        v-for="picture in pictures"
+        :key="picture"
+        :src="picture.src"
+      />
+    </v-carousel>
+
+    <v-card-title>
+      {{ title }}
+      <v-spacer></v-spacer>
+      <v-btn icon @click="show = !show">
+        <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
+      </v-btn>
+    </v-card-title>
+
+    <v-card-subtitle>{{ date }}</v-card-subtitle>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+        <v-card-text>{{ description }}</v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+      id: String,
+      title: String,
+      date: String,
+      description: String,
+      pictures: []
+  },
+  data: () => ({
+    show: false,
+  }),
+};
+</script>
